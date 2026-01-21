fcda8c3051e8db343a03b00f660f663d8c6eba5b
DanielSpiteri <146811711+DanielSpiteri@users.noreply.github.com>
2026-01-21 14:55:00 +0100

    Car Wash Dirt color
    Finally saved it after the unreal crashing bug

--- AdvancedGameDevPRoj/Intermediate/Build/BuildRulesProjects/AdvancedGameDevPRojModuleRules/obj/Development/AdvancedGameDevPRojModuleRules.AssemblyInfo.cs
+++ AdvancedGameDevPRoj/Intermediate/Build/BuildRulesProjects/AdvancedGameDevPRojModuleRules/obj/Development/AdvancedGameDevPRojModuleRules.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AdvancedGameDevPRojModuleRules")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Development")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e736b49f0e5a8ce81fe8dde5b05dc088e6cfea5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2900c2e10a5982c8ee98a05141c0e20f2493953f")]
 [assembly: System.Reflection.AssemblyProductAttribute("AdvancedGameDevPRojModuleRules")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AdvancedGameDevPRojModuleRules")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
